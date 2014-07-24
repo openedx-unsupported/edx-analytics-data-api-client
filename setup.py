55b73d84731977ea38b0f13e7367ae758d195123
Clinton Blackburn <clinton.blackburn@gmail.com>
2014-07-24 14:31:30 -0400

    Updated to version 0.2.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='edx-analytics-api-client',
-    version='0.2.3',
+    version='0.2.4',
     packages=['analyticsclient'],
     url='https://github.com/edx/edx-analytics-api-client',
     description='Client used to access edX analytics data warehouse',
