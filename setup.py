165431c2c9e21fa5cd394c33aaf21aaa56cf2481
Tyler Hallada <thallada@edx.org>
2017-01-04 11:41:52 -0500

    Update python package versions

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ setup(
     description='Client used to access edX analytics data warehouse',
     long_description=open('README.rst').read(),
     install_requires=[
-        "requests==2.2.0",
+        "requests==2.12.4",
     ],
     tests_require=[
-        "coverage==3.7.1",
-        "nose==1.3.3",
-        "httpretty==0.8.0",
-        "pep8==1.5.7",
-        "pylint==1.2.1",
-        "pep257==0.3.2"
+        "coverage==4.3.1",
+        "nose==1.3.7",
+        "httpretty==0.8.14",
+        "pep8==1.7.0",
+        "pylint==1.6.4",
+        "pep257==0.7.0"
     ]
 )
