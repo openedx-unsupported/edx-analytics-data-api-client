d8b351d0574db3af67c4418e43e345792c420cc0
John Eskew <jeskew@edx.org>
2018-02-12 17:44:12 -0500

    Add auto PyPI uploading to travis file, move version.

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
+from analyticsclient import __version__ as VERSION
 
 setup(
     name='edx-analytics-data-api-client',
-    version='0.14.0',
+    version=VERSION,
     packages=['analyticsclient', 'analyticsclient.constants'],
     url='https://github.com/edx/edx-analytics-data-api-client',
     description='Client used to access edX analytics data warehouse',
