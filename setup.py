1fe6a105ac6e5f56323c4722bca082fc3e2c5d76
Jeremy Bowman <jbowman@edx.org>
2020-03-10 10:19:30 -0400

    Fix deployment to PyPI

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
 
+import codecs
+import os
+import sys
 from setuptools import setup
 
 from analyticsclient import __version__ as VERSION
+
+
+if sys.argv[-1] == 'tag':
+    print("Tagging the version on github:")
+    os.system("git tag -a v%s -m 'v%s'" % (VERSION, VERSION))
+    os.system("git push --tags")
+    sys.exit()
+
+with codecs.open('README.rst', 'r', 'utf-8') as f:
+    LONG_DESCRIPTION = f.read()
 
 setup(
     name='edx-analytics-data-api-client',
@@ -10,13 +23,13 @@ setup(
     packages=['analyticsclient', 'analyticsclient.constants'],
     url='https://github.com/edx/edx-analytics-data-api-client',
     description='Client used to access edX analytics data warehouse',
-    long_description=open('README.rst').read(),
+    long_description=LONG_DESCRIPTION,
     license='Apache License 2.0',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache License 2.0',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
