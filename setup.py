6f16bea5ba4e76306cb26eb605e7d0bc42d071fa
Mohammad Ahtasham ul Hassan <ahthassan74@gmail.com>
2021-11-08 18:17:39 +0500

    build: standardize version number placement

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 import codecs
 import os
+import re
 import sys
 from setuptools import setup
-
-from analyticsclient import __version__ as VERSION
 
 
 if sys.argv[-1] == 'tag':
@@ -36,6 +35,21 @@ def is_requirement(line):
     that is, it is not blank, a comment, a URL, or an included file.
     """
     return line and not line.startswith(('-r', '#', '-e', 'git+', '-c'))
+
+
+def get_version(*file_paths):
+    """
+    Extract the version string from the file at the given relative path fragments.
+    """
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename).read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+VERSION = get_version("analyticsclient", "__init__.py")
 
 
 setup(
